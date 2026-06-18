5f6176e9c984276b0df3e0867bd1b6eabba1d2a3
fazliguler80 <151386322+fazliguler80@users.noreply.github.com>
2026-06-19 00:07:12 +0300

    Fix urls: remove admin_site import, use admin.site

--- sitem/urls.py
+++ sitem/urls.py
@@ -2,17 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from bina.admin import admin_site
 from bina import views
 
 urlpatterns = [
-    path('admin/login/', AdminLoginView.as_view(), name='admin_login'),
     path('admin/site-degistir/<int:site_id>/', views.site_degistir, name='site_degistir'),
     path('', views.home_page, name='home'),
     path('admin/', admin.site.urls),
-    path('portal/', include('bina.portal_urls')),  # bina.portal_urls'i include et
+    path('portal/', include('bina.portal_urls')),
 ]
 
-# Media dosyaları için (debug modunda)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
